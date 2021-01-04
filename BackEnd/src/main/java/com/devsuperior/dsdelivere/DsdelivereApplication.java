d96d67c9add050fb6e10b96328b3a221dcea880d
Nivaldo Sousa <nivaldol.sousa@bol.com.br>
2021-01-04 17:35:23 -0300

    projeto criado

---
+++ BackEnd/src/main/java/com/devsuperior/dsdelivere/DsdelivereApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdelivere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdelivereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdelivereApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/devsuperior/dsdelivere/DsdelivereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdelivere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdelivereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
